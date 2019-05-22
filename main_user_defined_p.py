2cf97ba7d02e809e0289f0337a89fc3507872041
Platinum <platinumemirate@gmail.com>
2019-05-22 11:36:20 +0100

    Add files via upload
    The varying p has a rather fixed value of p, it solves the acceptance probability for values of p ranging from 0.1 to 0.9. the value can be altered by changing the range in the code
    
    For the other part, the user can easily enter the value of p for which they are interested and other parameters.

---
+++ main_user_defined_p.py
@@ -0,0 +1,73 @@
+p = float(input("the proportion defective: "))
+c1 = int(input("C1: "))
+c2 = int(input("C2: "))
+n1 = int(input("Sample size 1: "))
+n2 = int(input("Sample size 2: "))
+q = 1 - p
+
+
+def permutation(n, x):
+    final = 0
+    for j in range(0, x + 1):
+        n_fac = 1
+        x_fac = 1
+        n_x_fac = 1
+        for i in range(1, n + 1):
+            n_fac = n_fac * i
+        for i in range(1, j + 1):
+            x_fac = x_fac * i
+        for i in range(1, n - j + 1):
+            n_x_fac = n_x_fac * i
+        p_x = p ** int(j)
+        q_x = q ** int(n - j)
+
+        the_fac = n_fac / (x_fac * n_x_fac)
+        final = final + (the_fac * p_x * q_x)
+
+    return final
+
+
+def permutation_fixed(n, j):
+    n_fac = 1
+    x_fac = 1
+    n_x_fac = 1
+    for i in range(1, n + 1):
+        n_fac = n_fac * i
+    for i in range(1, j + 1):
+        x_fac = x_fac * i
+    for i in range(1, n - j + 1):
+        n_x_fac = n_x_fac * i
+    p_x = p ** int(j)
+    q_x = q ** int(n - j)
+
+    the_fac = n_fac / (x_fac * n_x_fac)
+    fac = (the_fac * p_x * q_x)
+    return fac
+
+
+partial = 0
+for i in range(1, c2):
+    d1 = c1 + i
+    for_d1 = permutation_fixed(n1, d1)
+
+    d2 = c2 - (c1 + i)
+    for_d2 = permutation(n2, d2)
+
+    partial = partial + for_d1 * for_d2
+
+
+for1 = permutation_fixed(n1, c2)
+for2 = permutation(n2, 0)
+cons = for1 * for2 + partial
+
+pa1 = permutation(n1, c1)
+
+print("The Pa1 is: ")
+print(pa1)
+
+print("The Pa2 is: ")
+print(cons)
+
+pa = cons + pa1
+print("Thus the Probablity of acceptance Pa is ")
+print(pa)
